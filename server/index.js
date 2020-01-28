3b5ddeb98f16882775f10d50b8d0d3ab1b875269
deafult2010 <thomas.jaeger@blueyonder.co.uk>
2020-01-28 22:39:22 +0000

    Added socketio to client and server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-const cors = require('cors')
+const path = require('path');
+const cors = require('cors');
 
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-const config = require("./config/key");
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const config = require('./config/key');
 
 // const mongoose = require("mongoose");
 // mongoose
@@ -14,16 +16,20 @@ const config = require("./config/key");
 //   .then(() => console.log("DB connected"))
 //   .catch(err => console.error(err));
 
-const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI,
-  {
-    useNewUrlParser: true, useUnifiedTopology: true,
-    useCreateIndex: true, useFindAndModify: false
+const { Chat } = require('./models/Chat');
+
+const mongoose = require('mongoose');
+const connect = mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
   })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-app.use(cors())
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -31,25 +37,49 @@ app.use(cookieParser());
 
 app.use('/api/users', require('./routes/users'));
 
+io.on('connection', socket => {
+  socket.on('Input Chat Message', msg => {
+    connect.then(db => {
+      try {
+        let chat = new Chat({
+          message: msg.chatMessage,
+          sender: msg.userID,
+          type: msg.type
+        });
+
+        chat.save((err, doc) => {
+          if (err) return res.json({ success: false, err });
+
+          Chat.find({ _id: doc._id })
+            .populate('sender')
+            .exec((err, doc) => {
+              return io.emit('Output Chat Message', doc);
+            });
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  });
+});
 
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use('/uploads', express.static('uploads'));
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static('client/build'));
 
   // index.html for all page routes
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
 }
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server Running at ${port}`)
+server.listen(port, () => {
+  console.log(`Server Running at ${port}`);
 });
