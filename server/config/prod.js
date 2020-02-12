010945a25a984daccc8acc213dde41660b694d49
deafult2010 <thomas.jaeger@blueyonder.co.uk>
2020-02-12 23:06:21 +0000

    prepare for deploy

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,7 @@
+// module.exports = {
+//     mongoURI:process.env.MONGO_URI
+// }
 module.exports = {
-    mongoURI:process.env.MONGO_URI
-}
+  mongoURI:
+    'mongodb+srv://root:Flagex2010@cluster0-8beqf.mongodb.net/test?retryWrites=true&w=majority'
+};
